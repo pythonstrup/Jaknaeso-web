f52f3b5e56d092b849453ec315072173ddc3c138
suminhan123 <suminh324@gmail.com>
2025-02-03 22:56:11 +0900

    feat: server, client axios interceptor 토큰 세팅

--- src/app/(route)/(home)/page.tsx
+++ src/app/(route)/(home)/page.tsx
@@ -1,15 +1,11 @@
 import { Header } from '@/components/layout';
-import PrefetchHydration from '@/providers/PrefetchHydration';
-import testApis from '@/query-hooks/useTest/api';
-
-import { Test } from './components/Test';
 
 export default async function Home() {
   return (
-    <PrefetchHydration queryKey={['test']} queryFn={testApis.getTest}>
+    <>
       <Header />
-      <Test />
+
       <h3 className="title2">타이포 적용</h3>
-    </PrefetchHydration>
+    </>
   );
 }
