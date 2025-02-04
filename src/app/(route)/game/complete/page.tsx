c31032c32f3473cd0c6f6a14b7858ccb73c911c3
SeoYoung Bae <60652298+bsy1141@users.noreply.github.com>
2025-02-04 22:06:54 +0900

    feat: 가치관 게임 - 이중택일 페이지 퍼블리싱 (#25)
    * feat: 이중택일1,2 / 추가답변 퍼블리싱 완료
    
    * feat: 게임 완료 페이지 퍼블리싱

---
+++ src/app/(route)/game/complete/page.tsx
@@ -0,0 +1,44 @@
+'use client';
+import { useRouter } from 'next/navigation';
+
+import { Button } from '@/components/Button';
+
+import styles from './page.module.scss';
+
+export default function GameComplete() {
+  const router = useRouter();
+  return (
+    <div className={styles.container}>
+      <div className={styles.header}>
+        <h3 className="title2">
+          오늘의 질문
+          <br />
+          답변을 완료했어요!
+        </h3>
+        <p className={styles.subTitle}>내일도 답변을 완료해 내 캐릭터를 완성해 보세요</p>
+      </div>
+      <div className={styles.content}>
+        {/* 로티 이미지 들어갈 부분, 임시 조치 */}
+        <div
+          style={{
+            width: '220px',
+            height: '220px',
+            borderRadius: '1.625rem',
+            background: '#F8F8FA',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            color: '#6A6A6A',
+          }}
+        >
+          로티 애니메이션
+        </div>
+      </div>
+      <div className={styles.footer}>
+        <Button color="primary" onClick={() => router.push('/')}>
+          완료
+        </Button>
+      </div>
+    </div>
+  );
+}
--- src/assets/icons/arrows/index.ts
+++ src/assets/icons/arrows/index.ts
@@ -1,5 +1,7 @@
 import ArrowDown2Icon from './arrow-down2.svg';
 import ArrowLeftIcon from './arrow-left.svg';
 import ArrowLeft2Icon from './arrow-left2.svg';
+import ArrowRightIcon from './arrow-right2.svg';
 import ArrowUp2Icon from './arrow-up2.svg';
-export { ArrowDown2Icon, ArrowLeft2Icon, ArrowLeftIcon, ArrowUp2Icon };
+
+export { ArrowDown2Icon, ArrowLeft2Icon, ArrowLeftIcon, ArrowRightIcon, ArrowUp2Icon };
